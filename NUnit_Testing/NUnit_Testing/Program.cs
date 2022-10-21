bb78c33753999cafb69c376765f65674431fc933
Piyushnmj <piyushnmj@yahoo.com>
2022-10-21 21:05:09 +0530

    Square Root

--- NUnit_Testing/NUnit_Testing/Program.cs
+++ NUnit_Testing/NUnit_Testing/Program.cs
@@ -1,34 +1,11 @@
-﻿class TempConv
+﻿class SquareRoot
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Please select the unit conversion from below: \n1.Celsius-Fahrenheit 2.Fahrenheit-Celsius");
-        int userInput = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter a value: ");
+        double X = Convert.ToInt32(Console.ReadLine());
 
-        if (userInput == 1)
-        {
-            Console.WriteLine("\nEnter a value: ");
-            int val1 = Convert.ToInt32(Console.ReadLine());
-
-            int result1 = (val1 * 9 / 5) + 32;
-
-            Console.WriteLine("\n{0} °C is {1} °F", val1, result1);
-            Console.ReadLine();
-        }
-        else if (userInput == 2)
-        {
-            Console.WriteLine("\nEnter a value: ");
-            int val2 = Convert.ToInt32(Console.ReadLine());
-
-            int result2 = (val2 -32) * 5/9;
-
-            Console.WriteLine("\n{0} °F is {1} °C", val2, result2);
-            Console.ReadLine();
-        }
-        else
-        {
-            Console.WriteLine("\nInvalid Input, Try again...");
-            Console.ReadLine();
-        }
+        double SquareRoot = Math.Sqrt(X);
+        Console.WriteLine("\nThe Square Root of {0} = {1}", X, SquareRoot);
     }
 }
