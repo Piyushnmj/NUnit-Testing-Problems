9f61bd57647fe1cce495124d8680a47b110d43f5
Piyushnmj <piyushnmj@yahoo.com>
2022-10-23 17:13:56 +0530

    Vending Machine

--- NUnit_Testing/NUnit_Testing/Program.cs
+++ NUnit_Testing/NUnit_Testing/Program.cs
@@ -1,25 +1,36 @@
-﻿namespace MonthlyPayment
+﻿namespace VendingMachine
 {
     class Program
     {
+        public static void countCurrency(int amount)
+        {
+            int[] notes = new int[] { 1000, 500, 100, 50, 20, 10, 5, 2, 1 };
+            int[] noteCounter = new int[9];
+
+            for (int i = 0; i < 9; i++)
+            {
+                if (amount >= notes[i])
+                {
+                    noteCounter[i] = amount / notes[i];
+                    amount = amount % notes[i];
+                }
+            }
+
+            Console.WriteLine("Currency Count: ");
+            for (int i = 0; i < 9; i++)
+            {
+                if (noteCounter[i] != 0)
+                {
+                    Console.WriteLine(notes[i] + " : " + noteCounter[i]);
+                }
+            }
+        }
+
         public static void Main()
         {
-            Console.WriteLine("Enter Principal amount in Rs.: ");
-            double P = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("\nEnter Year to pay off principal amount: ");
-            double Y = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("\nEnter Rate of interest compounded monthly: ");
-            double R = Convert.ToInt32(Console.ReadLine());
-
-            double n = 12 * Y;
-            double r = R / (12 * 100);
-  
-            double payment = (P * r)/(1 - Math.Pow(1 + r, -n));
-
-            Console.WriteLine("The monthly payment to be paid for {0} years for a principle amount of Rs.{1} at {2}% interest compounded monthly is: Rs.{3}", Y, P, R, payment);           
-            Console.ReadLine();
+            Console.WriteLine("Enter amount: ");
+            int amount = Convert.ToInt32(Console.ReadLine());
+            countCurrency(amount);
         }
     }
 }
