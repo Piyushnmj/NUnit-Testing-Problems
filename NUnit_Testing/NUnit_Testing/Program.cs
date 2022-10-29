808ad360dec1de658736f35b0d06ae615bfa617e
Piyushnmj <piyushnmj@yahoo.com>
2022-10-29 11:50:59 +0530

    Convert Decimal values to Binary

--- NUnit_Testing/NUnit_Testing/Program.cs
+++ NUnit_Testing/NUnit_Testing/Program.cs
@@ -1,36 +1,24 @@
-﻿namespace VendingMachine
+﻿namespace DecimalToBinary
 {
     class Program
     {
-        public static void countCurrency(int amount)
+        static void Main(string[] args)
         {
-            int[] notes = new int[] { 1000, 500, 100, 50, 20, 10, 5, 2, 1 };
-            int[] noteCounter = new int[9];
-
-            for (int i = 0; i < 9; i++)
+            Console.WriteLine("Enter the Decimal Number : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int i;
+            int[] numberArray = new int[10]; //The Decimal numbers are the numbers whose base is 10. That means the decimal numbers range from 0 to 9.
+            for (i = 0; num > 0; i++)
             {
-                if (amount >= notes[i])
-                {
-                    noteCounter[i] = amount / notes[i];
-                    amount = amount % notes[i];
-                }
+                numberArray[i] = num % 2;
+                num = num / 2;
             }
-
-            Console.WriteLine("Currency Count: ");
-            for (int i = 0; i < 9; i++)
+            Console.WriteLine("\nBinary Represenation of the given Number : ");
+            for (i = i - 1; i >= 0; i--)
             {
-                if (noteCounter[i] != 0)
-                {
-                    Console.WriteLine(notes[i] + " : " + noteCounter[i]);
-                }
+                Console.Write(numberArray[i]);
             }
-        }
-
-        public static void Main()
-        {
-            Console.WriteLine("Enter amount: ");
-            int amount = Convert.ToInt32(Console.ReadLine());
-            countCurrency(amount);
+            Console.ReadLine();
         }
     }
 }
