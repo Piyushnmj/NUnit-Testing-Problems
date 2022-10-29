23a166b3a5e39729e5e3d5e5e9d83ae96b023171
Piyushnmj <piyushnmj@yahoo.com>
2022-10-29 12:02:06 +0530

    Swap Nibbles

--- NUnit_Testing/NUnit_Testing/Program.cs
+++ NUnit_Testing/NUnit_Testing/Program.cs
@@ -1,24 +1,18 @@
-﻿namespace DecimalToBinary
+﻿namespace SwapNibbles
 {
     class Program
     {
-        static void Main(string[] args)
+        // Function for swapping   
+        static int swapNibbles(int x)
         {
-            Console.WriteLine("Enter the Decimal Number : ");
-            int num = Convert.ToInt32(Console.ReadLine());
-            int i;
-            int[] numberArray = new int[10]; //The Decimal numbers are the numbers whose base is 10. That means the decimal numbers range from 0 to 9.
-            for (i = 0; num > 0; i++)
-            {
-                numberArray[i] = num % 2;
-                num = num / 2;
-            }
-            Console.WriteLine("\nBinary Represenation of the given Number : ");
-            for (i = i - 1; i >= 0; i--)
-            {
-                Console.Write(numberArray[i]);
-            }
-            Console.ReadLine();
+            return ((x & 0x0F) << 4 | (x & 0xF0) >> 4);
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine("Enter a Number to Swap: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nSwapped Number is: {0}",swapNibbles(x));
         }
     }
 }
